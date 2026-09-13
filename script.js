05922bf79948317b399372200934e6b0e4913d1e
JiannaShao <bluejjay25@gmail.com>
2026-09-13 13:11:36 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -242,7 +242,7 @@ resumeButton.addEventListener('click', async () => {
 
         const scene = new THREE.Scene();
 
-        /* Orthographic camera tilted 10 degrees left and 25 degrees lower. */
+        /* Orthographic camera tilted 10 degrees left and 15 degrees lower. */
         const camera = new THREE.OrthographicCamera(
           -6.7,
           6.7,
@@ -253,9 +253,9 @@ resumeButton.addEventListener('click', async () => {
         );
 
         camera.position.set(
-          -1.889,
-          -5.071,
-          10.710
+          -2.013,
+          -3.106,
+          11.415
         );
 
         camera.lookAt(
